9d108a544a4e6492546c28fbcad6b4f0f81c96a3
FJ-8 <fatjon.malushaj@gmail.com>
2024-04-16 15:04:00 +0200

    commit all chapters

---
+++ Chapter_3/notes.java
@@ -0,0 +1,40 @@
+/*
+* 1. variables can store two types of things: primitives and references
+* object state - instance variables
+* local variables - variables declared within a method
+* variables as arguments - values sent to a method by the calling code
+* return types values sent backj to the caller of the method.
+* primitive integer types (type int)
+*
+*
+* java cares about type
+ type safety
+ variables: primitive and object reference
+ primitives hold fundamental values (simple bit patterns): integers, booleans, and floating point numbers.
+ object references hold references to objects
+ variables must have a type and name
+ int count
+* A variable is just a vup a container it holds something
+*
+* it has a size and type. primitives
+* long 64 bit (huge to huge)
+* int  32 bit ( - 2147483648 to  147483647)
+* short 16 bit (- 32768 to 32767)
+* byte 8 bit (-128 to 127)
+* float 32 bit (varies)
+* double 64 bit(varies)
+* boolean char byte short int long float double
+*
+* there is actually no such thing as an object variable
+* there is only an object reference variable
+* an object reference variable
+* an object reference variable holds bits that represent way to access an object.
+* it doesn't hold the onject itself, but it holds something like a pointer. onr an adress. Except in java we don't really know what is inside
+* a reference variable, We do know that whatever it is, it represents one and only one object. And the JVM knows how to use the reference to get the object.
+* final assigns one object and that is the only object it can reference
+*
+*
+* an arrai is like a tray of cups
+* maps, trees, and sets
+*
+* */
---
+++ Chapter_4/notes.java
@@ -0,0 +1,26 @@
+/*
+* State affect behavior , behavior affects state.
+*
+* Objects Have STATE        and     BEHAVIOR represented by
+*       instance variables          mathods.
+*
+* methods use instance variable values,
+* Reminder: a class decribes what an object knows and what an object does
+* A class is the blueprint how the JVM should make an objet of that type.
+*
+* A caller PASSES ARGUMENTS . A method Takes parameters. (page 74 it makes all sense)
+*
+* argument - a value like 2, Foo, or a reference to a DOG
+* parameter is a local variable.
+*
+* If a method takes a parameter , you must pass it something when you call it.
+*
+* Java is pass-by-value That means pass-by-copy.
+*
+*
+*
+*
+*
+*
+*
+* */
